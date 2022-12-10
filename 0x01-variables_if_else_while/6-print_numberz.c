4c741583775f99697ad0d48b5de73c9b01190868
Stanhope Arthur <stanhope.c.arthur@gmail.com>
2022-12-10 05:24:01 +0100

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print single digits starting from 0
+ *
+ * Result: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+    for (i = 0; i < 20; i++)
+    {
+        putchar(i);
+    }
+    putchar('\n')
+    return (0);
+}
