fde282765a8defc1242f12635a408572b82262fa
Stanhope Arthur <stanhope.c.arthur@gmail.com>
2022-12-11 13:27:52 +0100

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 99.
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+    int i, e, g, h, lk1, lk2;
+    i = e = g = h = 48;
+    while (h < 58)
+    {
+        g = 48;
+        while (g < 58)
+        {
+            e = 48;
+            while (e < 58)
+            {
+                i=48;
+                while (i < 55)
+                {
+                    lk1 = (h * 10) + g;
+                    lk2 = (e * 10) + i;
+                    if (lk1 < lk2)
+                    {
+                        putchar(h);
+                        putchar(g);
+                        putchar(' ');
+                        putchar(e);
+                        putchar(i);
+                        if (h == 57 && g == 56 && e == 57 && i == 57)
+                            break;
+                        putchar(',');
+                        putchar(' ');
+                    }
+                    i++;
+                }
+                e++;
+            }
+            g++;
+        }
+        h++
+    }
+    putchar('\n');
+    return (0);
+}
