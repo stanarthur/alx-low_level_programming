2649ce42f1451bbdcdfe559aec5190607fc795cc
Stanhope Arthur <stanhope.c.arthur@gmail.com>
2022-12-13 20:36:36 +0100

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /** 
-* main - prints _putchar
+* main - prints putchar
 * Returns : always 0
 */
 int main(void)
@@ -14,6 +14,7 @@ int main(void)
     _putchar('h');
     _putchar('a');
     _putchar('r');
+    _putchar('\n');
 
     return (0);
 }
