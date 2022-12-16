8330798328161f73e34b217ef4ebf03b43ec2c3b
Stanhope Arthur <stanhope.c.arthur@gmail.com>
2022-12-16 01:58:50 +0100

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,15 @@
 
 /**
  * print_numbers - prints numbers between 0 to 9.
- * Return: Always 0 (Success)
+ * Return: no return.
  */
 void print_numbers(void)
 {
 	int ch;
 
-	for (ch = 0; ch < 10; ch++)
+	for (ch = 48; ch < 58; ch++)
 	{
-		_putchar(ch + '0');
+		_putchar(ch);
 	}
 	_putchar('\n');
 }
