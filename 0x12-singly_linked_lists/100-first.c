95970a5cfbaef1016d66cb88860a5cdd793f6d2c
Stanhope Arthur <stanhope.c.arthur@gmail.com>
2023-01-26 21:03:53 +0100

    fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * cmain - it prints a string before function 
- * executed before main
+ * cmain - it is the function executed before main
  * Return: it has no return.
  */
 
-void __attribute__((constructor)) bmain(void)
+void __attribute__((constructor)) cmain()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
