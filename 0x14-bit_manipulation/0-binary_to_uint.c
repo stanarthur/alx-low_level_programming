88d69d96e02ebaee6204514683ded353f45f8c4a
Stanhope Arthur <stanhope.c.arthur@gmail.com>
2023-02-02 18:24:45 +0100

    fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,8 @@ unsigned int binary_to_uint(const char *b)
 
 	for (len = 0; b[len] != '\0'; len++);
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (len--, base_two = 1; len >= 0; len--, 
+    base_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
 		{
