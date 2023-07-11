ff8f100383286949b4d8bb77584960a7f7dc012b
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2023-07-11 23:12:04 +0100

    13. Linear search in a skip list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -51,6 +51,7 @@ listint_t *jump_list(listint_t *list, size_t size, int value);
 listint_t *create_list(int *array, size_t size);
 void free_list(listint_t *list);
 void print_list(const listint_t *list);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 /*HELPER PROTOTYPES*/
 int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
