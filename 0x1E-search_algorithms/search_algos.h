717cb5ceb9629960cfa451963213ea9449bdf0fe
Stanhope Arthur <stanhope.c.arthur@gmail.com>
2023-09-11 12:59:25 +0100

    7. Jump search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -43,5 +43,6 @@ typedef struct skiplist_s
 /* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif
