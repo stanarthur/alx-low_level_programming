00b75913be6df1cd3e456f48343e43c11dbceda0
Stanhope Arthur <stanhope.c.arthur@gmail.com>
2023-09-13 23:45:14 +0100

    12. Jump search in a singly linked list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -45,8 +45,12 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
-int _binary_search(int *array, size_t left, size_t right, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+listint_t *create_list(int *array, size_t size);
+void free_list(listint_t *list);
+void print_list(const listint_t *list);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
